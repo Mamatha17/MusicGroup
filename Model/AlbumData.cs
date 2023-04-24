f3c4aedd92c1173a7e77570c9caefc11dfd32db6
Mamatha <mamatha.anumula@gmail.com>
2023-04-25 00:07:14 +0300

    Initial Commit

---
+++ Model/AlbumData.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicGroup.Model
+{
+    public class AlbumData
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string? Genre { get; set; }      
+        public string? Description { get;set; }
+        public string? Action { get; set; }
+
+    }
+}
